61d4450c2191cfeb586bc4d78e7aa9c0f56841d1
Sewon Kim <sewonkim0black@gmail.com>
2024-06-09 12:15:24 -0400

    Client: view activity form setup, name field not implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const nameRef = useRef(null);
+
   return (
     <div className="container">
-      <h1> TEST </h1>
+      {/* Header */}
+      <div className="header">
+        <h1> Stock Mirror </h1>
+        <p> Trade with confidence, trust, and guidance </p>
+        <div></div>
+      </div>
+
+      {/* Main */}
+      <div className="main">
+        <div className="bg-shade">
+          {/* View Activity */}
+          <form onSubmit={async (event) => {
+            event.preventDefault();
+
+            // send ajax to server
+            try {
+              const response = await fetch(`/api/getByName/Warren Buffet`, {
+                method: 'GET',
+                headers: {
+                  'Accept': 'application/json',
+                },
+              });
+              const data = await response.json();
+              console.log(data); // TEST
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+          }}>
+            {/* Name */}
+            <div className="form-name">
+              <label htmlFor="name"> Name: </label>
+              <input type="text" id="name" name="name" required />
+            </div>
+
+            {/* View Activity */}
+            <button type="submit"> View Activity </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
