e663d3d1639d565d74f3d51d4b86f4c75b64e8ca
yaojiejia <yaojiejia0715@gmail.com>
2024-06-08 23:33:19 -0400

    added error handlings

--- api/app.js
+++ api/app.js
@@ -4,16 +4,22 @@ import {spawn} from "child_process"
 const app = express();
 
 app.get("/getByName/:name", (req, res) => {
-    const name = req.params.name;
-    const pythonProcess = spawn('/usr/bin/python3',["scrape.py", name]);
-    //print the data
-    pythonProcess.stdout.on('data', (data) => {
-        const stocks = JSON.parse(data);
-        res.send(stocks);
-    });
-    // pythonProcess.on('exit', (code, signal) => {
-    //     console.log(`Python process exited with code ${code} and signal ${signal}`);
-    // });
+    if (req.params.name === "") {
+        res.send("No name provided");
+        return;
+    }
+    try {
+        const name = req.params.name;
+        const pythonProcess = spawn('/usr/bin/python3',["scrape.py", name]);
+        //print the data
+        pythonProcess.stdout.on('data', (data) => {
+            const stocks = JSON.parse(data);
+            res.send(stocks);
+        });
+    } catch (error) {
+        console.error(error);
+        res.send("Error occurred while scraping data: " + error);
+    }
 });
 
 
--- api/scrape.py
+++ api/scrape.py
@@ -5,55 +5,71 @@ import json
 import sys
 
 def fetch_html(url):
-    """Fetch HTML content from the given URL."""
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Connection': 'keep-alive'
-    }
-    response = requests.get(url, headers=headers)
-    return response.content
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Connection': 'keep-alive'
+        }
+        response = requests.get(url, headers=headers)
+        return response.content
+    except Exception as e:
+        print(e)
+        return [e]
 
 def parse_links(html_content):
-    """Parse links from HTML content."""
-    soup = BeautifulSoup(html_content, 'html.parser')
-    all_links = []
-    for i in range(1, 100):
-        a_elements = soup.select(f'#port_body > ul > li:nth-of-type({i}) > a')
-        links = [(a.get('href'), a.text.strip() if a.text else '') for a in a_elements]
-        fixed_links = [("https://www.dataroma.com/" + href, text) for href, text in links]
-        all_links.extend(fixed_links)
-    return all_links
+    try:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        all_links = []
+        for i in range(1, 100):
+                a_elements = soup.select(f'#port_body > ul > li:nth-of-type({i}) > a')
+                links = [(a.get('href'), a.text.strip() if a.text else '') for a in a_elements]
+                fixed_links = [("https://www.dataroma.com/" + href, text) for href, text in links]
+                all_links.extend(fixed_links)
+        return all_links
+    except Exception as e:
+        print(e)
+        return [e]
 
 def scrape_data_from_link(link):
-    """Scrape data from the given link."""
-    html_content = fetch_html(link)
-    soup = BeautifulSoup(html_content, 'html.parser')
-    data_rows = []
-    for i in range(1, 100):
-        td_elements = soup.select(f'#grid > tbody > tr:nth-of-type({i}) > td')
-        data = [td.text.strip() if td.text else '' for td in td_elements]
-        if data:
-            data_rows.append(data[1:])
-    return data_rows
+    try:
+        html_content = fetch_html(link)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        data_rows = []
+        for i in range(1, 100):
+            td_elements = soup.select(f'#grid > tbody > tr:nth-of-type({i}) > td')
+            data = [td.text.strip() if td.text else '' for td in td_elements]
+            if data:
+                data_rows.append(data[1:])
+        return data_rows
+    except Exception as e:
+        print(e)
+        return [e]
 
 def scrape_links(investor_name):
-    """Scrape links containing the given investor's name."""
-    html_content = fetch_html("https://www.dataroma.com/m/home.php")
-    all_links = parse_links(html_content)
-    investor_links = [(href, name) for href, name in all_links if investor_name in name]
-    return investor_links
+    try:
+        html_content = fetch_html("https://www.dataroma.com/m/home.php")
+        all_links = parse_links(html_content)
+        investor_links = [(href, name) for href, name in all_links if investor_name in name]
+        return investor_links
+    except Exception as e:
+        print(e)
+        return [e]
 
 def scrape_data_for_investor(investor_name):
-    """Scrape data for the given investor."""
-    investor_links = scrape_links(investor_name)
-    data = []
-    for href, name in investor_links:
-        data.extend(scrape_data_from_link(href))
+    try:
+        investor_links = scrape_links(investor_name)
+        data = []
+        for href, name in investor_links:
+            data.extend(scrape_data_from_link(href))
 
-    return json.dumps(data)
+        return json.dumps(data)
+    except Exception as e:
+        print(e)
+        return [e]
+
 
 
 
